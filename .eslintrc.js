7fae8430676313e62ac097869497dad129500937
Matti Bispham <matti@bispham.io>
2024-04-04 09:22:08 +0900

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+//https://github.com/awelzel/zeekjs-misp/blob/main/.eslintrc.js
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  globals: {
+    // easy way out
+    zeek: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': 'off',
+  },
+};
