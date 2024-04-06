8b6e24c1f3a4782443ea1c3f1dd5cb1744353884
mbispham <matti@bispham.io>
2024-04-06 20:28:44 +0900

    Add plugin config

---
+++ plugin/src/Plugin.cc
@@ -0,0 +1,17 @@
+#include "config.h"
+#include "Plugin.h"
+
+namespace zeek::plugin::zeekjs_redis { Plugin plugin; }
+
+using namespace zeek::plugin::zeekjs_redis;
+
+zeek::plugin::Configuration Plugin::Configure()
+        {
+        zeek::plugin::Configuration config;
+        config.name = "zeekjs::redis";
+        config.description = "TODO: Insert description";
+        config.version.major = VERSION_MAJOR;
+        config.version.minor = VERSION_MINOR;
+        config.version.patch = VERSION_PATCH;
+        return config;
+        }
